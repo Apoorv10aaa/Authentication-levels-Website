684359ff087d1301510b24d7adaf8c2ddb37531c
Apoorv Srivastava <9415577008aaa@gmail.com>
2023-06-29 17:30:52 +0530

    Level 1 Authentication

---
+++ app.js
@@ -0,0 +1,59 @@
+const express= require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+
+
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema={
+    email: String,
+    password: String
+}
+
+const User= new mongoose.model("User",userSchema);
+
+
+app.get("/",function(req,res){
+    res.render("home");
+})
+
+app.get("/login",function(re,res){
+    res.render("login");
+})
+
+app.post("/login",async function(req,res){
+    const name=req.body.username;
+    const password=req.body.password;
+
+    const user= await User.findOne({email:name});
+    if(user){
+        if(user.password===password){
+            res.render("secrets");
+        }
+    }
+
+})
+
+app.get("/register",function(req,res){
+    res.render("register");
+})
+
+app.post("/register",function(req,res){
+    const newUser=new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    newUser.save();
+    res.render("secrets");
+
+})
+
+app.listen(3000,function(){
+    console.log("Server running at port 3000");
+})
