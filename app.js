71d0efd5ff10e72bad2bab202331b938b08885d8
Apoorv Srivastava <9415577008aaa@gmail.com>
2023-06-30 14:50:46 +0530

    level 2-password encryption without env variable

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express= require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption"); 
 
 
 const app=express();
@@ -11,10 +12,13 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const userSchema={
-    email: String,
-    password: String
-}
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+const secret="ThisIsMySecret";
+userSchema.plugin(encrypt, { secret: secret,encryptedFields: ["password"]});
 
 const User= new mongoose.model("User",userSchema);
 
