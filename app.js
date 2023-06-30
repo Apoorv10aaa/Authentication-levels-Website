765027153a244fc1f1973fc4f99205c68cf37737
Apoorv Srivastava <9415577008aaa@gmail.com>
2023-06-30 15:21:23 +0530

    Level 2 - .env file added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express= require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
@@ -17,8 +18,7 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-const secret="ThisIsMySecret";
-userSchema.plugin(encrypt, { secret: secret,encryptedFields: ["password"]});
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User= new mongoose.model("User",userSchema);
 
