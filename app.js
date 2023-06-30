351c950287777d8c1ea356171db751af260b996c
Apoorv Srivastava <9415577008aaa@gmail.com>
2023-07-01 01:59:41 +0530

    Level 3-Hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express= require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption"); 
+const md5 = require('md5');
 
 
 const app=express();
@@ -18,8 +18,6 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
-
 const User= new mongoose.model("User",userSchema);
 
 
@@ -33,7 +31,7 @@ app.get("/login",function(re,res){
 
 app.post("/login",async function(req,res){
     const name=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     const user= await User.findOne({email:name});
     if(user){
@@ -51,7 +49,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save();
     res.render("secrets");
